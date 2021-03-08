c7f10cd837a7464563fd19d7b28b878fd3336763
CHUNG-CHIA CHENG <36121454+Ratherman@users.noreply.github.com>
2021-03-08 14:34:38 +0800

    Update the functionality of the main file

--- Chapter01_Introduction/Frequency_Histogram.py
+++ Chapter01_Introduction/Frequency_Histogram.py
@@ -1,44 +1,46 @@
-import pandas
+import pandas as pd
 import os
 
 class presenter:
 
-    def __init__(self):
-        pass
+    def __init__(self): pass
     
-    # Input: 
-    #     file_name: the file containing the data.
-
-    # Output: 
-    #     dataOut: a list of data.
+    # Input: file_name <string> the file containing the data.
+    # Output: dataOut <list> a list of data.
     def getData(self, file_name):
+        # Check the file is exist
         exist = self.check_file_exist(file_name)
-        print(exist)
 
+        # The dataType is DataFrame.
+        data = pd.read_csv("data.csv")
 
-    def printData(self):
-        pass
+        # The dataType is transformed to List
+        data_list = []
+        for datum in data["1"]: data_list.append(datum)
+        return data_list
 
-    def makeFrequency(self):
-        pass
+    # Input: data_list <list> It contains data.
+    # Output: None
+    def printData(self, data_list):
+        print(data_list)
 
+    # Input: data_list <list> It contains data.
+    # Output: data_freq <dict> It contains statistic info - frequency of data.
+    def makeFrequency(self, data_list):
+        data_freq = {ele: data_list.count(ele) for ele in data_list}
+        return data_freq
 
-    def makeHistogram(self):
-        pass
+    # Input: dataFreq <dict> It contains statistic info - frequency of data.
+    # Output: None
+    def makeHistogram(self, data_freq, file_name):
+        print("The following is the frequency of the data in {file_name}.")
+        for i in range(1,21): print(f"{i}: {data_freq[i]*'*'}")
 
+    # Input: file_name <String> It contains data in csv form.
+    # Output: result <Boolean> It checks whether the file exist.
+    def check_file_exist(self, file_name): return file_name in os.listdir()
 
-
-    # Inner Methods
-
-    # Input:
-    #    file_name: the file containing the data.
-
-    # Output:
-    #    resutle: true/false
-
-    def check_file_exist(self, file_name):
-        return file_name in os.listdir()
-
-
-        
-
+presenter = presenter()
+data = presenter.getData("data.csv")
+data_freq = presenter.makeFrequency(data)
+presenter.makeHistogram(data_freq, "data.csv")
